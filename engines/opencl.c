6a7e365b8e56bfb654153f4941d9a638afe59632
John Wiele <jwiele@redhat.com>
2023-09-07 22:28:46 +0000

    Checkpoint. Semi-working.

--- engines/opencl.c
+++ engines/opencl.c
@@ -18,68 +18,93 @@
 
 static char buffer[BUFFER_SIZE] = { 0 };
 
+
+enum memtype {
+	MEMTYPE_HOST = 1,
+	MEMTYPE_DEVICE,
+};
+
 struct opencl_options {
 	struct thread_data *td;
-	cl_platform_id platform_id;
-	cl_device_id device_id;
 	cl_context context;
 	cl_mem buffer;
 	cl_command_queue command_queue;
+	enum memtype memtype;
+	void *host_buffer;	/* If memtype == MEMTYPE_HOST */
 };
 
 static struct fio_option options[] = {
-
+	{
+		.name  = "memtype",
+		.lname = "type of memory access, host or device",
+		.type  = FIO_OPT_STR,
+		.off1  = offsetof(struct opencl_options, memtype),
+		.def   = "host",
+		.posval = {
+			{
+				.ival = "host",
+				.oval = MEMTYPE_HOST,
+				.help = "Use host memory", 
+			},
+			{
+				.ival = "device",
+				.oval = MEMTYPE_DEVICE,
+				.help = "Use GPU device memory",
+			},
+		},
+	},
+	{
+		.name	 = NULL,
+	},
 };
 
 static pthread_mutex_t running_lock = PTHREAD_MUTEX_INITIALIZER;
 
 static unsigned int running = 0;
 
+static cl_platform_id platform_id;
+static cl_device_id device_id;
+
+
 static int init_devices(struct thread_data *td)
 {
-	struct opencl_options *o = td->eo;
 	cl_int result;
 
 	/* Get the first platform. */
-	result = clGetPlatformIDs(1, &o->platform_id, NULL);
+	result = clGetPlatformIDs(1, &platform_id, NULL);
 	if (result != CL_SUCCESS) {
 		log_err("No platform found: %d", result);
 			return 1;
 	}
 
 	/* Get the first device of the default type on the platform. */
-	result = clGetDeviceIDs(o->platform_id, CL_DEVICE_TYPE_DEFAULT, 1,  &o->device_id, NULL);
+	result = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1,  &device_id, NULL);
 	if (result != CL_SUCCESS) {
 		log_err("No device found: %d", result);
 			return 1;
 	}
 
-	/* Create a context with default properties. */
-	/* TODO: add a callback for error reporting. */
-	o->context = clCreateContext( NULL, 1, &o->device_id, NULL, NULL, &result);
-	if (result != CL_SUCCESS) {
-		log_err("CreateContext failed: %d", result);
-			return 1;
-	}
-
-	/* Create a command queue */
-	o->command_queue =
-		clCreateCommandQueueWithProperties(o->context, o->device_id, NULL, &result);
-	if (result != CL_SUCCESS) {
-		log_err("CreateCommandQueueWithProperties failed: %d", result);
-			return 1;
-	}
 	return 0;
 }
 
 static enum fio_q_status fio_opencl_queue(struct thread_data *td,
 					  struct io_u *io_u)
-{						\
+{
 	struct opencl_options *o = td->eo;
 	cl_int result;
 	
+	/* Create a command queue */
+	if (!o->command_queue) {
+		o->command_queue =
+			clCreateCommandQueueWithProperties(o->context, device_id, NULL, &result);
+		if (result != CL_SUCCESS) {
+			log_err("CreateCommandQueueWithProperties failed: %d", result);
+			return 1;
+		}
+	}
+
 	result = clEnqueueWriteBuffer(o->command_queue, o->buffer, CL_TRUE, 0,
-				      sizeof(buffer), buffer, 0, NULL, NULL);
+				      io_u->buflen, buffer, 0, NULL, NULL);
 	if (result != CL_SUCCESS) {
 		log_err("CreateCommandQueueWithProperties failed: %d", result);
 			return 1;
@@ -96,6 +121,8 @@ static int fio_opencl_init(struct thread_data *td)
 			 pthread_mutex_unlock(&running_lock);
 			 return 1;
 		 }
+	 } else {
+		 
 	 }
 	 running++;
 	 pthread_mutex_unlock(&running_lock);
@@ -105,13 +132,42 @@ static int fio_opencl_init(struct thread_data *td)
 static int fio_opencl_iomem_alloc(struct thread_data *td, size_t total_mem)
 {
 	struct opencl_options *o = td->eo;
+	cl_mem_flags flags;
 	int result;
 
-	/* Create a memory buffer on the device. */
-	o->buffer = clCreateBuffer(o->context, CL_MEM_READ_WRITE, total_mem, NULL, &result);
+	td->orig_buffer = calloc(1, total_mem);
+	if (!td->orig_buffer) {
+		log_err("Orig Buffer allocation failed\n");
+		return 1;
+	}
+	if (o->memtype == MEMTYPE_HOST) {
+		/* Create a memory buffer on the host. */
+		flags = CL_MEM_USE_HOST_PTR;
+		o->host_buffer = calloc(1, total_mem);
+		if (!o->host_buffer) {
+			log_err("Host buffer allocation failed");
+			return 1;
+		}
+	} else {
+		/* Create a memory buffer on the device. */
+		flags = CL_MEM_READ_WRITE;
+		o->host_buffer = NULL;
+	}
+
+	/* Create a context with default properties. */
+	/* TODO: add a callback for error reporting. */
+	if (!o->context) {
+		o->context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &result);
+		if (result != CL_SUCCESS) {
+			log_err("CreateContext failed: %d", result);
+			return 1;
+		}
+	}
+
+	o->buffer = clCreateBuffer(o->context, flags, total_mem, o->host_buffer, &result);
 	if (result != CL_SUCCESS) {
 		log_err("CreateBuffer failed: %d", result);
-			return 1;
+		return 1;
 	}
 	return 0;
 }
